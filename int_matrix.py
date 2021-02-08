814db4ae33cdcdc8c7716c68acae1dc84c5f4a3d
tevidebaek <71346095+tevidebaek@users.noreply.github.com>
2021-02-08 14:36:29 -0500

    Added new versions of scripts.
    Changed the data structure within `generate_all_matrices` to be indices of non-zero elements instead of `numpy` arrays. This gives the same run time while give a large improvement in the use of memory. To allow users to run the old versions those functions have been left in and a boolean flag `element_run` which allows you to toggle between the two data structures.

--- int_matrix.py
+++ int_matrix.py
@@ -5,8 +5,11 @@ import time
 #import pyximport; pyximport.install()
 #import cy_int_matrix_func as imf
 import int_matrix_func as imf
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #import multiprocessing as mp
+#import sys
+import gc
+#import os, psutil
 
 #for each group of three columns we can only choose three total elements. If any of those choices are not in location (i,i)
 #then it restricts one of the choices in the next group of three columns. Maybe the easiest way to do this is to brute force
@@ -15,70 +18,57 @@ import matplotlib.pyplot as plt
 start = time.time()
 
 #how many species do we want to have
-k=5
+k=6
 save_src = './k'+str(k)+'-valid-tiling/'
 save_csv = 'k'+str(k)+'-intmatrices.csv'
 save_to_csv = False
 save_img = False
+element_run = False
 
-interaction_matricies = imf.generate_all_matrices(k)
+if element_run:
+    interaction_matricies = imf.generate_all_matrices_elements(k)
+else:
+    interaction_matricies = imf.generate_all_matrices(k)
 
 #we should now be done with the loop
 print('all* int matrices', len(interaction_matricies))
-    
-#decimated_matricies = imf.remove_bad_line_interactions(interaction_matricies, k)
 
-#print('removed bad lines', len(decimated_matricies))
-
-#decimated_matricies = interaction_matricies
+#process = psutil.Process(os.getpid())
+#print('memory used', process.memory_info().rss)
 
 valid_loops = []
 
-#color_pool = mp.Pool()
-
-#def check_color(M):
-#    return imf.valid_coloring(M,k)
-
-#color_bool = color_pool.map(check_color, interaction_matricies)
-
-#valid_loops = interaction_matricies[color_bool]
-#for M in decimated_matricies:
-for M in interaction_matricies:    
-    if imf.valid_coloring(M,k): valid_loops.append(M)
+if element_run:
+    for e_M in interaction_matricies:
+        #print(np.array(e_M).T)
+        #M = imf.generate_m_from_elements(e_M, k)
+        M = imf.generate_m_from_indices(e_M,k)
+        #print(M)
+        if imf.valid_coloring(M,k): valid_loops.append(M)
+        
+else:
+    for M in interaction_matricies:
+        if imf.valid_coloring(M,k): valid_loops.append(M)
 
 print('removed bad loops', len(valid_loops))
 
 non_degen_list = imf.remove_degenerate_matrices(valid_loops, k)
-#non_degen_list = imf.remove_degenerate_matrices(decimated_matricies, k)
       
 print('removed degenerate matrices', len(non_degen_list))
-
-#final_list = []
-
-#for M in non_degen_list:
-    
-#    if imf.valid_coloring(M,k): final_list.append(M)
-
-#print('removed bad loops', len(final_list))
 
 final_list = non_degen_list
 
 for i in range(len(final_list)):
     imf.draw_int_matrix(final_list[i], save_src, i,save=save_img, show=True)
+
     fname = save_src+'k'+str(k)+'-intmatrix-'+str(i)+'.csv'
-    print(fname)
+    #print(fname)
     if save_to_csv:
         np.savetxt(fname, final_list[i].astype(int),delimiter=',')
         
 print(time.time() - start)
         
-run_times = np.array([0.122,0.376,1.506,15.6,847.07,15601])
+#run_times = np.array([0.122,0.376,1.506,15.6,847.07,15601])
 
-#plt.figure()
-#plt.scatter([2.,3.,4.,5.,6.,7.], run_times, c='k')
-#plt.semilogy()
-#plt.ylabel('run time [s]', fontsize=16)
-#plt.xlabel('k-species', fontsize=16)
-#plt.show()
-
+gc.collect()
         
--- int_matrix_func.py
+++ int_matrix_func.py
@@ -2,6 +2,8 @@ import numpy as np
 import itertools
 import matplotlib.pyplot as plt
 import time
+import os, psutil
+
 
 #want to make drawings of the interaction matricies
 def draw_int_matrix(M, img_dir='./', num_m=0, save=False, show=False):
@@ -114,6 +116,7 @@ def get_element_info(M):
         side = (i-3*c_s)%3
 
         element_info.append([c_s, r_s, side])
+
 
     return np.array(element_info)
     
@@ -395,12 +398,18 @@ def generate_all_matrices(k):
                 if np.sum(M_p[col_indx, col_indx])<3:
                     if check_bad_lines(M_p,k):
                         new_int_matricies.append(M_p)
+                        
+                
     
                 #for new_m in new_int_matrices:
                 #    print(new_m)
     
         #after looping through all existing Ms we replace the list with the new list
         interaction_matricies = new_int_matricies.copy()
+        
+        process = psutil.Process(os.getpid())
+        print('memory used (MB)', process.memory_info().rss/1000000.)
+        
         
         #interate col_indx
         col_set_indx += 1
@@ -434,8 +443,205 @@ def check_bad_lines(M, k):
             #print(set_diag)
             if np.sum(set_diag)==2:
                 set_props.append(np.where(set_diag==0)[0][0])
+            #if np.sum(set_diag)==3:
+            #    if k>2: return False #removes two-line tilings from list
                 
     if len(np.unique(set_props))<2:  
         return True
-    else: return False
     
+    else: 
+        #print(set_props)
+        #print(M)
+        return False
+
+def check_bad_lines_elements(M, k):
+    #here M is assumed to be the list of indices for non-zero elements
+    
+    set_props = []
+    
+    for i in range(k):
+        for j in range(i,k):
+            set_indx = [[0+3*i, 0+3*j],[1+3*i, 1+3*j],[2+3*i, 2+3*j]]
+            #print(set_indx)
+            diag_count = 0
+            #pos = []
+            
+            for l in range(3):
+                if set_indx[l] in M: diag_count +=1
+                else: #pos.append(i) 
+                    pos=l
+                
+            #print(set_diag)
+            if diag_count==2:
+                set_props.append(pos)
+
+    if len(np.unique(set_props))<2:
+        #print(set_props)
+        return True
+    
+    else: 
+        #print(set_props)
+        #print(M)
+        return False
+
+def generate_m_from_elements(element_info, k):
+    
+    M = np.zeros((3*k, 3*k))
+    
+    if len(element_info)==0: return M
+    
+    for element in element_info:
+        M[int(3*element[0]+element[2]), int(3*element[1]+element[2])]=1
+        
+    return M
+
+def get_element_index(element_info,k):
+    indices = []
+    
+    if len(element_info)==0: return [None, None]
+    
+    for element in element_info:
+        indices.append([3*element[0]+element[2], 3*element[1] + element[2]])
+    return indices
+
+def generate_m_from_indices(indices,k):
+    M = np.zeros((3*k, 3*k))
+    for ind in indices: M[ind[0], ind[1]]=1
+    return M
+
+def generate_all_matrices_elements(k):
+    #start with a list of all the matrices we will eventually make
+    interaction_matricies = []    
+
+    #M = np.zeros((3*k, 3*k))  #initialize our first matrix
+    #interaction_matricies.append(get_element_info(M))
+    
+    interaction_matricies.append([])
+    
+    ### end initilization of variables ###
+        
+    #need a counter for the column set that we are looking at. A column set are the group of three columns that correspond to
+    #the interactions of a single particle species
+    col_set_indx = 0    
+        
+    while col_set_indx<k:
+        
+        print('current col', col_set_indx, ' of ', k, ', list length', len(interaction_matricies))
+        #print(col_set_indx)
+        
+        #available columns in set
+        col_indx = np.array([0,1,2])+3*col_set_indx
+        #print(col_indx)
+        
+        new_int_matricies = [] 
+        
+        #look at each existing matrix to see what new matricies we could make from these
+        for M in interaction_matricies:
+            
+            #generate a matrix to manipulate
+            #M = generate_m_from_elements(e_M, k)  #want to rewrite this section to remove this step of generating a matrix from the elements
+            
+            indices = np.array(M)
+            #print(indices.T)
+            
+            #we want to see which columns in the column set of M do not have elements already.
+            col_to_fill = []
+
+            if len(indices)==0: col_to_fill = list(col_indx)
+            else:
+                for col in col_indx:
+                    if col in indices.T[0]: continue
+                    else: col_to_fill.append(col)
+            
+            #col_to_fill = []
+            #for col in col_indx: #looping through available columns
+            #    if len(M[col])>0: continue
+            #    else: col_to_fill.append(col)
+                   
+            #if all columns of set are full then readd the matrix and go to next one, no additional elements to add
+            if len(col_to_fill)==0:
+                new_int_matricies.append(M)
+                continue
+                
+            #if there are elements to add, do that next
+            #print("columns to fill", col_to_fill)
+            
+            #for each column we have k choices, and in the set these choices do not interfer with each other
+            combs = list(itertools.product(range(k), repeat=len(col_to_fill)))  #get all combinations of [0,...,k-1]
+            #print(combs)
+            
+            #for each combination we need to create a new matrix
+            for i in combs:
+                i_copy = M.copy()
+    
+                #print(type(i_copy))
+                #print(M_p)
+                
+                #fill as many rows as the length of col_to_fill
+                row_to_fill = []
+                
+                if len(indices)==0:
+                    for j in range(len(col_to_fill)):
+                        row_to_fill.append(col_to_fill[j]%3 + 3*i[j])
+                
+                else:
+                    for j in range(len(col_to_fill)):
+                        #need to choose the correct row to fill. Recall that we will only be filling the diagonal elements of the row sets
+                        row = col_to_fill[j]%3 + 3*i[j]
+                    
+                        if row in indices.T[1]: continue
+                        else: row_to_fill.append(row)
+                        
+                        #check if row is already filled
+                        #if any(M_p[row])>0: continue
+                        #else: row_to_fill.append(row)
+                
+                #if no rows to fill from this combination, skip it
+                if len(row_to_fill)!=len(col_to_fill): continue
+                
+                #print('row to fill', row_to_fill)
+                
+                for col, row in zip(col_to_fill, row_to_fill):
+                    i_copy.append([col,row])
+                    if row!=col: i_copy.append([row,col])
+                
+                #for col, row in zip(col_to_fill, row_to_fill):
+                #    M_p[col, row]=1
+                #    M_p[row, col]=1
+                
+                diag_count=0
+                for col in col_indx:
+                    if [col,col] in i_copy: diag_count+=1
+            
+                if diag_count<3:
+                    if check_bad_lines_elements(i_copy, k):
+                        new_int_matricies.append(i_copy)
+            
+                        #test_M = generate_m_from_indices(i_copy, k)
+                        #
+                        #if np.sum(test_M[col_indx, col_indx])<3:
+                        #    if check_bad_lines(test_M,k): print('correct bad line check')
+                        #    else: 
+                        #        print(test_M)
+                        #        print(i_copy)
+                                
+                                
+                #if np.sum(M_p[col_indx, col_indx])<3: #removes only self-interacting triangles
+                #    if check_bad_lines(M_p,k): #removes matrices that have intersecting line patterns
+                #        new_int_matricies.append(get_element_info(M_p))
+    
+                #for new_m in new_int_matrices:
+                #    print(new_m)
+    
+        #after looping through all existing Ms we replace the list with the new list
+        interaction_matricies = new_int_matricies.copy()
+        
+        process = psutil.Process(os.getpid())
+        print('memory used (MB)', process.memory_info().rss/1000000.)
+        
+        #interate col_indx
+        col_set_indx += 1
+        
+        #for m in interaction_matricies:
+        #    print(interaction_matricies)
+    return interaction_matricies
